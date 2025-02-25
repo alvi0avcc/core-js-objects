feb1a2cc86fb49c92d66ab1aafc74555009a714a
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2025-02-25 10:21:28 +0300

    feat: makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -120,8 +120,16 @@ const makeImmutable = (obj) => Object.freeze(obj);
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  if (Object.keys(lettersObject).length === 0) return '';
+  const maxIndex = Math.max(...Object.values(lettersObject).flat());
+  const resultArray = new Array(maxIndex + 1).fill('');
+  Object.entries(lettersObject).forEach(([letter, positions]) => {
+    positions.forEach((position) => {
+      resultArray[position] = letter;
+    });
+  });
+  return resultArray.join('');
 }
 
 /**
